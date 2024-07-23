a3ea523edad8b7b772e6c41ade950b124c4e66d4
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2024-07-23 15:00:39 +0000

    Your commit message

--- components/components/ui/bento-grid.tsx
+++ components/components/ui/bento-grid.tsx
@@ -46,8 +46,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "MongpDB", "Typescript"];
+  const rightLists = ["JavaScript", "NextJS", "AWS"];
 
   const [copied, setCopied] = useState(false);
 
