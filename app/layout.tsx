4be4561fdf9256354ccd3bbda53d94e67ec56058
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-03-17 00:11:44 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="qrqMKXHY7-3iZVaIxU8pwP3AEBzT9mX3BtW-67YB9Iw" />
         <link rel="icon" href="/resume.svg" sizes="any" />
       </head>
       <body className={inter.className}>
